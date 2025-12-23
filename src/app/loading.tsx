866a450c7f25ef6232d4608a0a405828bf915ac6
Anisha Kumari <anishamukhiya2004@gmail.com>
2025-12-23 14:08:41 +0000

    still not showing

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,6 +3,5 @@
 import { LoadingScreen } from '@/components/loading-screen';
 
 export default function Loading() {
-  // You can add any UI inside Loading, including a Skeleton.
   return <LoadingScreen />;
 }
