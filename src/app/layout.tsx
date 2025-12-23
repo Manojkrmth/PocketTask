50f418a22dc6a896d793fe8131705cab37a95858
Anisha Kumari <anishamukhiya2004@gmail.com>
2025-12-23 18:36:43 +0000

    dark mode remove ksro bckground se and jo new do button add kiye ho same

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,13 +30,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-neutral-800">
+      <body className="font-body antialiased bg-neutral-100">
         <CurrencyProvider>
           <main className="max-w-md mx-auto bg-background min-h-screen relative pb-24 shadow-2xl">
             <Suspense fallback={children}>
