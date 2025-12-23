4183375dd7ee7aa5f15ed91d9a7837b851d8fcc3
Anisha Kumari <anishamukhiya2004@gmail.com>
2025-12-23 14:09:07 +0000

    add new loading screen now

--- src/components/loading-screen.tsx
+++ src/components/loading-screen.tsx
@@ -12,7 +12,7 @@ export function LoadingScreen() {
           "text-white"
         )}>
         <div className="relative h-24 w-24">
-            <div className="absolute inset-0.5 animate-spin rounded-full bg-gradient-to-r from-primary to-accent" />
+            <div className="absolute -inset-0.5 animate-spin rounded-full bg-gradient-to-r from-primary to-accent" />
             <div className="relative flex h-full w-full items-center justify-center rounded-full bg-neutral-900">
                 <Image 
                     src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEjQeXPPDoYHtSI3CkEycSr99eEzj5eNNnXZkkzetdCk8G5qhltxgm9vXYe4O2nRb8eJIkTRvSW7WljNX1U4sgGJopouCKxTr_u6Vn6eG5mmZrFt9Fw2R9L_VgCzk4J3BLhQu9UG7uAuGy3INawPoZlC1j11YSD0TSRCnUglyTByJM2ajI_Ce8O2t1d9Ahk/s320/photo_2025-11-21_17-20-41.jpg"
@@ -25,7 +25,7 @@ export function LoadingScreen() {
         </div>
         <div className="flex items-center gap-3 text-center">
             <Loader2 className="h-5 w-5 animate-spin text-primary" />
-            <p className="text-lg font-medium text-neutral-300">Loading app...</p>
+            <p className="text-lg font-medium text-neutral-300">Loading App...</p>
         </div>
       </div>
     );
